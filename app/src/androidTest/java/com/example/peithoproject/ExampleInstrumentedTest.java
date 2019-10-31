86dd06754e1af8d9e307ec755c4b2276b8e228c7
Tony Calarese <accalarese@gmail.com>
2019-10-30 22:07:00 -0400

    Attempted protype layout for XML
    Not too sure on what to use for the data visualization part as I am not sure as to what type of chart we are using

--- app/src/androidTest/java/com/example/peithoproject/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/peithoproject/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.peithoproject;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
