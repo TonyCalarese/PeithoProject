8e23a062ba06624d8f486fcf341b77c334d5f3be
Tony Calarese <accalarese@gmail.com>
2019-11-12 16:20:33 -0500

    working on the camera
    working on the camera part of the app but the app keeps crashing for some reason going to troubleshoot this mpre later tonight

--- app/src/main/java/com/example/peithoproject/MainActivity.java
+++ app/src/main/java/com/example/peithoproject/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.peithoproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.drawable.DrawableCompat;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mCameraButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCameraButton = (Button) findViewById(R.id.cameraButton);
+        mCameraButton.setOnClickListener(new View.OnClickListener() {
+                                             @Override
+                                             public void onClick(View view) {
+                                                 //Code Source: https://stackoverflow.com/questions/13977245/android-open-camera-from-button
+                                                 Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                                                 startActivity(intent);
+                                             }
+                                         }
+        );
+
+
+
     }
 }
