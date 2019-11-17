a3c835c659f3cf0d0deeb84b1c3af11b04a47479
Tony Calarese <accalarese@gmail.com>
2019-11-17 15:38:00 -0500

    worked on the camera code

--- app/src/main/java/com/example/peithoproject/MainActivity.java
+++ app/src/main/java/com/example/peithoproject/MainActivity.java
@@ -1,32 +1,23 @@
 package com.example.peithoproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button mCameraButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mCameraButton = (Button) findViewById(R.id.cameraButton);
-        mCameraButton.setOnClickListener(new View.OnClickListener() {
-                                             @Override
-                                             public void onClick(View view) {
-                                                 //Code Source: https://stackoverflow.com/questions/13977245/android-open-camera-from-button
-                                                 Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                                                 startActivity(intent);
-                                             }
-                                         }
-        );
 
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
-
+        if (fragment == null) {
+            fm.beginTransaction().add(R.id.fragment_container, new MainMenuFragement()).commit();
+        }
     }
 }
---
+++ app/src/main/java/com/example/peithoproject/MainMenuFragement.java
@@ -0,0 +1,86 @@
+package com.example.peithoproject;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+
+import java.io.File;
+import java.util.List;
+
+
+public class MainMenuFragement extends Fragment {
+    private static final String MAIN_MENU_STATIC = "TEST";
+    private static final int REQUEST_PHOTO = 2;
+
+    private Button mPhotoButton;
+    private File mPhotoFile;
+    private ImageView mPhotoView;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.prototype_layout_1, container, false);
+        PackageManager packageManager = getActivity().getPackageManager();
+
+
+        mPhotoButton = (Button) v.findViewById(R.id.cameraButton);
+        final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        boolean canTakePhoto = mPhotoFile != null && captureImage.resolveActivity(packageManager) != null;
+        mPhotoButton.setEnabled(canTakePhoto);
+
+        mPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = FileProvider.getUriForFile(getActivity(),
+                        "com.bignerdranch.android.criminalintent.fileprovider",
+                        mPhotoFile);
+                captureImage.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+
+                List<ResolveInfo> cameraActivities = getActivity()
+                        .getPackageManager().queryIntentActivities(captureImage,
+                                PackageManager.MATCH_DEFAULT_ONLY);
+
+                for (ResolveInfo activity : cameraActivities) {
+                    getActivity().grantUriPermission(activity.activityInfo.packageName,
+                            uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                }
+
+                startActivityForResult(captureImage, REQUEST_PHOTO);
+            }
+        });
+
+        mPhotoView = (ImageView) v.findViewById(R.id.cameraPreview);
+        updatePhotoView();
+        return v;
+    }
+
+
+    private void updatePhotoView() {
+        if (mPhotoFile == null || !mPhotoFile.exists()) {
+            mPhotoView.setImageDrawable(null);
+        } else {
+            Bitmap bitmap = PictureUtils.getScaledBitmap(
+                    mPhotoFile.getPath(), getActivity());
+            mPhotoView.setImageBitmap(bitmap);
+        }
+    }
+
+
+}
