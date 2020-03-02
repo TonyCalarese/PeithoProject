8767efc7f0389b374a1655399357e37ddd10e5a2
Tony Calarese <accalarese@gmail.com>
2020-03-02 16:14:11 -0500

    almost got recylcer working
    almost got  it working but the display is not working properly for some reason

--- app/src/main/java/com/example/peithoproject/DataFrame.java
+++ app/src/main/java/com/example/peithoproject/DataFrame.java
@@ -2,6 +2,7 @@ package com.example.peithoproject;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,13 +13,20 @@ public class DataFrame extends Peitho {
 
     //Recycler
     private class DataHolder extends RecyclerView.ViewHolder{
+        private TextView mEmoView;
 
         private int mPosition;
         public DataHolder(LayoutInflater inflater, ViewGroup container) {
             super(inflater.inflate(R.layout.tile, container, false));
+            mEmoView = (TextView) itemView.findViewById(R.id.DataTile);
+            mEmoView.setText("No Emotion Detected");
         }
         public void bindPosition(int p) {
             mPosition = p;
+        }
+
+        public void appendData() {
+            //Add Model Class Syncornous get function here
         }
     }
 
