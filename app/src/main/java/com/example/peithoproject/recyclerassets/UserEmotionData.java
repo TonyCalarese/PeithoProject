1de70c1c203f9197f6acc1aba61c54dfaf607063
Tony Calarese <accalarese@gmail.com>
2020-03-03 18:01:46 -0500

    Factored EmoHolder to UserEmotionalData
    EmoHolder is now User Emotional Data instead of Being Seperate

--- app/src/main/java/com/example/peithoproject/recyclerassets/UserEmotionData.java
+++ app/src/main/java/com/example/peithoproject/recyclerassets/UserEmotionData.java
@@ -1,21 +1,25 @@
 package com.example.peithoproject.recyclerassets;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserEmotionData {
-    private List<String> mEmotions ;
+    private List<String> mEmotions = new ArrayList<>();;
 
     public UserEmotionData() {
         mEmotions.add("Neutral");
     }
 
     //Getters
-    public List<String> getAllEmotions() {
+    public synchronized List<String> getAllEmotions() {
         return mEmotions;
     }
-    public int getEmotionDataSize() { return mEmotions.size();}
+    public synchronized int getEmotionDataSize() { return mEmotions.size();}
 
     //setters
-    public void add(String emotion){ mEmotions.add(emotion);}
+    public synchronized void add(String emotion){ mEmotions.add(emotion);}
+    public synchronized String getEmo() {
+        return mEmotions.get(mEmotions.size() - 1);
+    }
 
 }
