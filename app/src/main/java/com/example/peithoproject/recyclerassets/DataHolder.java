f442d1c467db5e6a69b3fd6f5270b92e5e3a9a68
Tony Calarese <accalarese@gmail.com>
2020-04-23 17:17:06 -0400

    Updated Manifest, Holder, and ChartsActivity

--- app/src/main/java/com/example/peithoproject/recyclerassets/DataHolder.java
+++ app/src/main/java/com/example/peithoproject/recyclerassets/DataHolder.java
@@ -1,12 +1,16 @@
 package com.example.peithoproject.recyclerassets;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.peithoproject.R;
+
+import static androidx.core.content.ContextCompat.startActivity;
 
 //Recycler
 public class DataHolder extends RecyclerView.ViewHolder{
@@ -16,13 +20,22 @@ public class DataHolder extends RecyclerView.ViewHolder{
     public DataHolder(LayoutInflater inflater, ViewGroup container) {
         super(inflater.inflate(R.layout.saved_speech_item, container, false));
         mSpeechButton = (Button) itemView.findViewById(R.id.speech_button);
+        mSpeechButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = ViewSavedChart.newIntent(this, mSpeechButton.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 
     public void bindPosition(String name, int p) {
         mPosition = p;
         mSpeechButton.setText(name);
     }
-
 }
 
 
--- app/src/main/java/com/example/peithoproject/recyclerassets/ViewSavedChart.java
+++ app/src/main/java/com/example/peithoproject/recyclerassets/ViewSavedChart.java
@@ -1,36 +1,50 @@
-package com.example.peithoproject;
+package com.example.peithoproject.recyclerassets;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.example.peithoproject.R;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class ViewSavedChart extends AppCompatActivity {
+    //FileName
+    private static final String FILE= "test.txt"; //Setting the file name upon loading from intent
+    private String mFile;
     //Charting
     public LineChart mChart;
     ArrayList<Entry> mHappinessData = new ArrayList<>(); //Charting data for Happiness
     ArrayList<Integer> mGatheredEmotions = new ArrayList<>();
 
+    public static Intent newIntent(Context packageContext, String file) {
+        Intent intent = new Intent(packageContext, ViewSavedChart.class);
+        intent.putExtra(FILE, file);
+        return intent;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_saved_chart);
+        Intent getFileName = getIntent();
+
+        if(getFileName != null)
+        {
+            mFile = getFileName.getStringExtra(FILE); // Get the File Name from the Intent
+        }
 
         //Charting
         mChart = (LineChart) findViewById(R.id.LineChart);
         mChart.setTouchEnabled(true);
         mChart.setPinchZoom(true);
-
 
         mGatheredEmotions.add(0); //Initial value for testing
 
